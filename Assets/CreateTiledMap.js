61bebf6d57f6393cc28bdcb5094ff3022c9e480a
Lisergishnu <marco.benzi@alumnos.usm.cl>
2015-10-08 15:30:46 -0300

    Terrain is now generated

--- Assets/CreateTiledMap.js
+++ Assets/CreateTiledMap.js
@@ -1,34 +1,56 @@
 ﻿#pragma strict
 
-var grassTile : GameObject;
-var dirtTile : GameObject;
 var width : int;
 var height : int;
 var seed : int;
-var xOffset : int;
-var yOffset : int;
-var zOffset : int;
 var granularity : float;
 var heightOffset : float;
+var mesh : Mesh;
+
 
 function Start () {
+
+	mesh = new Mesh();
+	mesh.name = "Map Terrain";
+	GetComponent.<MeshFilter>().mesh = mesh;
+
+	var newVertices : Vector3[] = new Vector3[(width+1)*(height+1)];
+	var newUV : Vector2[];
+	var newTriangles : int[] = new int[width * height * 6];
+
 	Random.seed = seed;
-	for (var i = 0; i < width ; i++)
+	var k : int = 0;
+	var ti : int = 0;
+	var vi : int = 0;
+	for (var j = 0; j <= height ; j++)
 	{
-		for (var j = 0; j < height; j++)
+		for (var i = 0; i <= width; i++)
 		{
 			var r = Mathf.PerlinNoise(i*granularity,j*granularity);
-			var position = Vector3(i*xOffset,r*yOffset,j*zOffset);
-			if (r > heightOffset) 
-			{
-				Instantiate(dirtTile, position, Quaternion.identity);
-			}	
-			else 
-			{
-				Instantiate(grassTile, position, Quaternion.identity);
-			}
+			var position = new Vector3(i,r,j);
+			newVertices[k] = position;
+			k++;
 		}
 	}
+	mesh.vertices = newVertices;
+	
+	for (j = 0; j < height ; j++)
+	{
+		for (i = 0; i < width; i++)
+		{
+			newTriangles[ti] = vi;
+			newTriangles[ti + 3] = newTriangles[ti + 2] = vi + 1;
+			newTriangles[ti + 4] = newTriangles[ti + 1] = vi + width + 1;
+			newTriangles[ti + 5] = vi + width + 2;
+			ti += 6;
+			vi++;
+		}
+		vi++;
+	}
+	
+	mesh.triangles = newTriangles;
+	mesh.RecalculateNormals();
+
 }
 
 function Update () {

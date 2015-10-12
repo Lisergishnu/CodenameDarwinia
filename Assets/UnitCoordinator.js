e78121843d59925e5f5459d46294ba83d4731ec2
Lisergishnu <marco.benzi@alumnos.usm.cl>
2015-10-12 17:54:36 -0300

    Unit selection tweaked, now working in four directions

--- Assets/UnitCoordinator.js
+++ Assets/UnitCoordinator.js
@@ -32,10 +32,19 @@ function Update () {
 		}
 		var rt = selectionBox.transform as RectTransform;
 		var sd = Input.mousePosition - startingScreenPoint;
-		rt.sizeDelta = Vector2(sd.x,-sd.y);
+		rt.pivot = Vector2(
+			(sd.x < 0) ? 1 : 0,
+			(sd.y < 0) ? 1 : 0);
+			
+		rt.sizeDelta = Vector2(
+			(sd.x < 0) ? -sd.x : sd.x,
+			(sd.y < 0) ? -sd.y : sd.y);
+
 	} else if (draggingBox) {
 		draggingBox = false;
 		var endPoint = screenToIsometric(Input.mousePosition.x, Input.mousePosition.y);
 		selectionBox.transform.position = offscreenPos;
+		rt = selectionBox.transform as RectTransform;
+		rt.sizeDelta = Vector2(0,0);
 	}
 }

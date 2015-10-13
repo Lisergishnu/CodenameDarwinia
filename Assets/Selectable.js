5b946fe8c4a0ef1563275bfcf0d81ffe8306b9b0
Lisergishnu <marco.benzi@alumnos.usm.cl>
2015-10-13 00:29:24 -0300

    Selection Outline

--- Assets/Selectable.js
+++ Assets/Selectable.js
@@ -1,9 +1,11 @@
 ﻿#pragma strict
 
 var selected : boolean = false;
-
+var width : float = 0.03;
+var offShader : Shader;
+var onShader : Shader;
 function Start () {
-
+	
 }
 
 function Update () {
@@ -12,10 +14,12 @@ function Update () {
 
 function OnSelection() {
 	Debug.Log("Unit found and selected");
+	GetComponent.<Renderer>().material.shader = onShader;
 	selected = true;
 }
 
 function OnUnselection() {
 	Debug.Log("Unit deselected");
+	GetComponent.<Renderer>().material.shader = offShader;
 	selected = false;
 }

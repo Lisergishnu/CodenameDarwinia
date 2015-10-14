1776f4b0a84e0b27e5c1507de25f8109d3fa3313
Lisergishnu <marco.benzi@alumnos.usm.cl>
2015-10-14 13:29:54 -0300

    Added objective script and prefab

---
+++ Assets/Objective.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Objective(name:String, pos:Vector2) {
+	this.name = name;
+	var hit : RaycastHit;
+	if (Physics.Raycast(Vector3(pos.x,100,pos.y),Vector3.down,hit))
+	{
+		this.transform.position = hit.point;
+		this.transform.position.y += 1;
+	} else {
+	Debug.LogError("Couldn't position objective!");
+	}
+	
+}
+
+function Start () {
+	
+}
+
+function Update () {
+
+}

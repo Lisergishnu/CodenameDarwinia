ec01a9ad82c186c6ccffc5c5fdceb0bff82be4d4
Lisergishnu <marco.benzi@alumnos.usm.cl>
2015-10-18 21:54:22 -0300

    Added robustness to moving back ai state

--- Assets/AIState/MovingBackAIState.js
+++ Assets/AIState/MovingBackAIState.js
@@ -17,8 +17,13 @@ public class MovingBackAIState extends EnemyAIState {
 	}
 	
 	function CheckDistance() {
-		Debug.Log(Vector3.Distance(entity.gameObject.transform.position, entity.initialPosition));
-		if (Vector3.Distance(entity.gameObject.transform.position, entity.initialPosition) < 4.2) {
+		//Hack
+		var v1 : Vector3 = entity.gameObject.transform.position;
+		v1.y = 0;
+		var v2 : Vector3 = entity.initialPosition;
+		v2.y = 0;
+		Debug.Log(Vector3.Distance(v1,v2));
+		if (Vector3.Distance(v1,v2) < 3) {
 			Debug.Log("Returned home.");
 			entity.ChangeState(IdleAIState(entity));
 		}

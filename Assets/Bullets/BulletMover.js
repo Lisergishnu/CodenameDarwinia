dcbfae228cc7290f484af5945b4e20a5f22a4039
Lisergishnu <marco.benzi@alumnos.usm.cl>
2015-10-18 23:12:46 -0300

    Now bullets and better enemy highlighting

---
+++ Assets/Bullets/BulletMover.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var currentTarget : GameObject;
+var initialPos : Vector3;
+var speed : float = 1;
+var t : float = 0;
+
+function Start () {
+	initialPos = transform.position;
+}
+
+function Update () {
+	if (!currentTarget)
+		Debug.LogError("Bullet instancied without target!!");
+		
+	t += Time.deltaTime*speed;
+	if (t >= 1) {
+		currentTarget.SendMessage("OnBulletHit");
+		Destroy(this.gameObject,0.1f);
+		return;
+	} else {
+		transform.position = Vector3.Lerp(initialPos, currentTarget.transform.position, t);
+	}
+}

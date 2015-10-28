80cf35a29b9f6140a4ed2376985da5112cca42ed
Lisergishnu <marco.benzi@alumnos.usm.cl>
2015-10-28 00:04:31 -0300

    Camera rotation and programatic look

--- Assets/IsometricRTSCamera.js
+++ Assets/IsometricRTSCamera.js
@@ -2,26 +2,29 @@
 
 var speed : float;
 var zoomSpeed : float;
+var cameraDistance : int = 20;
 private var mCamera: Camera;
+private var lookingDir : Vector3;
 
 function Start () {
 	// Show starting area
 	var sA : GameObject = GameObject.Find("Starting Area");
 	mCamera = GetComponent.<Camera>();
-	
+	lookingDir = sA.transform.position;
+	LookAt(lookingDir);
 }
 
 function LookAt(worldPoint: Vector3) {
-	
+	transform.position = worldPoint - transform.forward*cameraDistance;
 }
 
-function RotateCameraClockwise() {
+function RotateCameraCounterClockwise() {
 	transform.Rotate(-45,0,0);
 	transform.Rotate(0,90,0);
 	transform.Rotate(45,0,0);
 }
 
-function RotateCameraCounterClockwise() {
+function RotateCameraClockwise() {
 	transform.Rotate(-45,0,0);
 	transform.Rotate(0,-90,0);
 	transform.Rotate(45,0,0);
@@ -41,11 +44,17 @@ function Update () {
 	translationXZ *= Time.deltaTime;
 	translationY *= Time.deltaTime;
 	
-	var dirXZ : Vector3 =  transform.forward;
-	dirXZ.Normalize();
-	dirXZ *= translationXZ;
+	var dirN : Vector3 =  Vector3.ProjectOnPlane(transform.forward,Vector3.up);
+	dirN.Normalize();
+	dirN *= translationY;
 	
-	transform.Translate(dirXZ.x, translationY, dirXZ.z);
+	var dirE : Vector3 = Vector3.ProjectOnPlane(transform.right, Vector3.up);
+	dirE.Normalize();
+	dirE *= translationXZ;	
+	
+	var dirT = dirN + dirE;
+	lookingDir += dirT;
+	LookAt(lookingDir);
 	
 	var translationZoom = Input.GetAxis("Zoom") * zoomSpeed * Time.deltaTime;
 	var oS = mCamera.orthographicSize + translationZoom;
